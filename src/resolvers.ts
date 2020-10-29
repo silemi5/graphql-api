0a176cf289540a9c6c57e4803a7d81007186962a
silemi5 <emirjojr5@gmail.com>
2020-10-29 10:29:43 +0800

    feat: creates user refactor: removed createUser function

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,19 +2,21 @@ import { User } from './models/user'
 
 export const resolvers = {
   Query: {
-    users: () => {
-      return getAllUsers()
+    users: async () => {
+      // TODO: Must be protected route
+      return await User.find({})
     }
   },
   Mutation: {
-    createUser: (ctx)
+    createUser: async (_: null, { input }: { input: { email: string; password: string; } }) => {
+      try {
+        return await User.create({
+          email: input.email,
+          password: input.password
+        })
+      } catch {
+        throw new Error(`Something happened~`)
+      }
+    }
   }
-}
-
-async function createUser(ctx) {
-  // TODO: Create user
-}
-
-async function getAllUsers() {
-  return await User.find({})
 }
