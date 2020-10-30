9de300b03740377f2430a515b58d87cc90b5e97c
silemi5 <emirjojr5@gmail.com>
2020-10-30 17:56:11 +0800

    test: implements test for creating a new user test: removes old test spec

--- test/features/create-user.feature
+++ test/features/create-user.feature
@@ -3,6 +3,5 @@ Feature: Create user
   Scenario: Create a new user
     Given I entered "example@example.com" as my email and "password1" as my password
     When we try to create a new user
-    Then we should receive
-      | id          | name | email               |
-      | <Object ID> | null | example@example.com |
+    Then we should have given an ID, email, and name
+    And email should match with what has given at the start
--- test/features/step_definitions/create-user.steps.ts
+++ test/features/step_definitions/create-user.steps.ts
@@ -1,36 +1,52 @@
-import { Before, Given, When, Then } from 'cucumber'
-import { User } from '../../../src/models/user'
-import { server } from '../../../src/server'
+import { Given, When, Then } from 'cucumber'
 import axios from 'axios'
-import dotenv from 'dotenv'
-import { assert } from 'console'
+import chai from "chai";
 
-dotenv.config();
+const expect = chai.expect;
 
-let email: string
-let password: string
-let response
+
 const uri = 'http://localhost:4000'
-const JWT_SECRET: string = process.env.JWT_SECRET || ""
+const input: { email: string; password: string; name?: string } = { email: '', password: ''}
+let res: { _id: string; email: string; password: string; name?: string }
+
+const instance = axios.create({
+  baseURL: uri,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
 
 
 Given('I entered {string} as my email and {string} as my password', async function (string: string, string2: string) {
-  email = string;
-  password = string2;
+  input.email = string
+  input.password = string2;
 })
 
 
 When('we try to create a new user', async function() {
-  const query = `mutation: createUser(input: { email: $email, password: $password}) { name email }`
+  const query = `
+    mutation createUser($input: CreateUserInput!) {
+      createUser(input: $input) {
+        id
+        name
+        email
+      }
+    }
+  `
+  const response = await instance.post('/graphql', {
+    query: query,
+    variables: { "input": input }
+  })
 
-  response = await axios.post(uri, JSON.stringify({
-    "query": query,
-    "variables": { "email": email, "password": password}
-  }))
-  
-  // throw new Error('Not implemented')
+  if(!response) throw new Error("Something happened!")
+
+  res = response.data.data.createUser
 })
 
-Then('we should receive', function () {
-  throw new Error('Not implemented')
+Then('we should have given an ID, email, and name', async function () {
+  expect(res).to.have.all.keys('id', 'email', 'name');
 })
+
+Then('email should match with what has given at the start', async function () {
+  expect(res).to.include({ "email": input.email, "name": null });
+});
