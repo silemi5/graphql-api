686dff654d002d904c05cc587e7e9b238eb05fcf
silemi5 <emirjojr5@gmail.com>
2020-11-03 19:03:53 +0800

    feat: authentication user refactor: consolidate userScheme to user model file

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,25 @@
-import mongoose from 'mongoose';
-import { userSchema } from '../schemas/user'
+import mongoose, { Document } from 'mongoose';
+
+export type UserDocument = {
+  _id: string;
+  name?: string;
+  email: string;
+  password?: string;
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    select: true
+  },
+  password: {
+    type: String,
+    select: false
+  },
+})
+
+export default mongoose.model<Document & UserDocument>('User', userSchema)
 
 
-export const User = mongoose.model('User', userSchema)
+
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,25 +1,29 @@
-import { User } from './models/user'
+import UserModel, { UserDocument } from './models/user'
 import { sign } from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const JWT_SECRET: string = process.env.JWT_SECRET || ""
+const DB_URI: string = process.env.CONNECTION_URI || ""
+
+mongoose.connect(DB_URI)
 
 export const resolvers = {
   Query: {
     users: async () => {
       // TODO: Must be protected route
-      return await User.find({})
+      return await UserModel.find({})
     },
     user: async (_: null, id: string) => {
       // TODO: Must be protected route
-      return await User.find({ id: id })
+      return await UserModel.find({ id: id })
     },
     authenticateUser: async (_: null, { input }: { input: { email: string, password: string } }) => {
-      const user: User = await User.find(input)
+      const user: any = await UserModel.find(input)
 
-      if(!user) throw new Error("Invalid credentials!")
+      if(!user || user === []) throw new Error("Invalid credentials!")
 
       return {
         token: sign({
@@ -35,10 +39,7 @@ export const resolvers = {
   Mutation: {
     createUser: async (_: null, { input }: { input: { email: string; password: string; } }) => {
       try {
-        return await User.create({
-          email: input.email,
-          password: input.password
-        })
+        return await UserModel.create(input)
       } catch {
         throw new Error(`Something happened~`)
       }
